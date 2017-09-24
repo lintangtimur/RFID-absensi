cd8b5cc43a1bffd5bb3beaac4d19cd8efd2d847b
Lintang Timur <lintangtimur@protonmail.com>
2017-09-24 18:50:08 +0700

    add view function

--- core/helper/function.php
+++ core/helper/function.php
@@ -8,3 +8,13 @@ function dd($variable)
 {
     die(var_dump($variable));
 };
+
+/**
+ * VIEW
+ * @param  string $viewer View no logic
+ * @return string
+ */
+function view(string $viewer)
+{
+    return 'view/'.$viewer.".view.php";
+}
